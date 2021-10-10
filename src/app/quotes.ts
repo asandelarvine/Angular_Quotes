ba2f9ec8827dd312d002888031e8dd5436f3e6d4
asandelarvine <larvine.asande@student.moringaschool.com>
2021-10-10 13:32:58 +0300

    button added

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,8 +1,10 @@
 export class Quotes {
+    show-details!:boolean;
   
     constructor(  public name:string,
         public author:string,
         public quote:string){
+            this.show-details = false;
 
     }
 
