e3bb50e20fb3748854bbe7fbf3a8330bbf80d5bc
asandelarvine <larvine.asande@student.moringaschool.com>
2021-10-11 01:20:26 +0300

    final commits

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,12 +8,12 @@ import { Quotes } from '../quotes';
 })
 export class QuotesComponent implements OnInit {
   quotes:Quotes[]=[
-    new Quotes("Larvine","Burt Rutan","Testing leads to failure, and failure leads to understanding",0,0),
-    new Quotes("Larvine","Chris Pine","Programming is not about what you know; it is about what you can figure out",0,0),
-    new Quotes("Larvine","Dennis Ritchie","The only way to learn programming language is by writing programs in it",0,0),
-    new Quotes("Larvine","Unknown","When I wrote this Code, only God and I understood what I did.Now only God knows ",0,0),
-    new Quotes("Larvine","Unknown","Copy-and-Paste was programmed by programmers for programmers actually",0,0),
-
+    new Quotes("Publisher:  Larvine","Author:  Burt Rutan","Testing leads to failure, and failure leads to understanding",0,0),
+    new Quotes("Publisher:  Larvine","Author:  Chris Pine","Programming is not about what you know; it is about what you can figure out",0,0),
+    new Quotes("Publisher:  Larvine","Author:  Dennis Ritchie","The only way to learn programming language is by writing programs in it",0,0),
+    new Quotes("Publisher:  Larvine","Author:  Unknown","When I wrote this Code, only God and I understood what I did.Now only God knows ",0,0),
+    new Quotes("Publisher:  Larvine","Author:  Unknown","Copy-and-Paste was programmed by programmers for programmers actually",0,0),
+ 
   ];
   show(index:any){
     this.quotes[index].show = !this.quotes[index].show;
