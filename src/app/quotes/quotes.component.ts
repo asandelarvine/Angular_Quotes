b7853ed1a8d8154e92acbf7180e5ff212ff67496
asandelarvine <larvine.asande@student.moringaschool.com>
2021-10-11 16:29:19 +0300

    dates changed

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,11 +8,11 @@ import { Quotes } from '../quotes';
 })
 export class QuotesComponent implements OnInit {
   quotes:Quotes[]=[
-    new Quotes("Publisher:  Larvine","Author:  Burt Rutan","Testing leads to failure, and failure leads to understanding",new Date(2015,5,4),0,0),
-    new Quotes("Publisher:  Larvine","Author:  Chris Pine","Programming is not about what you know; it is about what you can figure out",new Date(2010,5,9),0,0),
-    new Quotes("Publisher:  Larvine","Author:  Dennis Ritchie","The only way to learn programming language is by writing programs in it",new Date(2000,9,1),0,0),
-    new Quotes("Publisher:  Larvine","Author:  Unknown","When I wrote this Code, only God and I understood what I did.Now only God knows ",new Date(2001,3,9),0,0),
-    new Quotes("Publisher:  Larvine","Author:  Unknown","Copy-and-Paste was programmed by programmers for programmers actually",new Date(2015,8,4),0,0),
+    new Quotes("Publisher:  Larvine","Author:  Burt Rutan","Testing leads to failure, and failure leads to understanding",new Date(2019,5,4),0,0),
+    new Quotes("Publisher:  Larvine","Author:  Chris Pine","Programming is not about what you know; it is about what you can figure out",new Date(2021,5,9),0,0),
+    new Quotes("Publisher:  Larvine","Author:  Dennis Ritchie","The only way to learn programming language is by writing programs in it",new Date(2020,9,1),0,0),
+    new Quotes("Publisher:  Larvine","Author:  Unknown","When I wrote this Code, only God and I understood what I did.Now only God knows ",new Date(2021,10,9),0,0),
+    new Quotes("Publisher:  Larvine","Author:  Unknown","Copy-and-Paste was programmed by programmers for programmers actually",new Date(2021,8,4),0,0),
  
   ];
   show(index:any){
